3028441589a8f180ca0cc827865b897bb67df807
Zohre Salimi <zohre.1987@gmail.com>
2020-01-01 17:07:43 +0100

    render ui

--- server.js
+++ server.js
@@ -1,33 +1,37 @@
-const express = require('express')
-const bodyParser = require('body-parser')
+const express = require("express")
+const bodyParser = require("body-parser")
 const app = express()
-const port = 3000;
-const path = require('path')
+const port = 3000
+const path = require("path")
 
-
-const { processProduct } = require('./main')
+const { processProduct } = require("./main")
 
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
-app.use(express.static('view'))
+app.use(express.static("view"))
 
-app.get('/', (request, response) => {
-    response.sendFile(path.resolve('view/index.html'))
+app.get("/", (request, response) => {
+	response.sendFile(path.resolve("view/index.html"))
 })
 
-app.post('/process', async (req, res) => {
-    const responseJson = {
-        ok: true
-    }
-    var processedProduct = await processProduct(req.body.url).catch(error => {
-        responseJson.status = false
-    })
+app.post("/process", async (req, res) => {
+	const responseJson = {}
+	var productDetail = await processProduct(req.body.url).catch(error => {
+		responseJson.status = false
+		responseJson.data = error
+	})
 
-    if (processProduct) {
-        responseJson.data = processedProduct
-    }
+	console.log(productDetail)
 
-    res.json(responseJson)
+	if (!productDetail) {
+		responseJson.status = false
+		responseJson.data = "Could not find product"
+	} else {
+		responseJson.status = true
+		responseJson.data = productDetail
+	}
+
+	res.json(responseJson)
 })
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
--- view/assets/app.js
+++ view/assets/app.js
@@ -1,25 +1,78 @@
-async function paste(e) {
-    e.preventDefault();
-    const processBtn = document.querySelector('.button-wrapper button');
-    var pasteText = document.querySelector(".url-input");
-    const text = await navigator.clipboard.readText();
-    pasteText.textContent = text;
-    if (pasteText.textContent) {
-        processBtn.disabled = false;
-    }
+const processBtn = document.querySelector(".button-wrapper button")
+const form = document.getElementById("form")
+const textContainerElement = document.querySelector(".text-container")
 
-    console.log(text);
+async function paste(e) {
+	e.preventDefault()
+	var pasteText = document.querySelector(".url-input")
+	const text = await navigator.clipboard.readText()
+	pasteText.textContent = text
+	if (pasteText.textContent) {
+		processBtn.disabled = false
+	}
+
+	console.log(text)
 }
-const pasteBtn = document.querySelector('#paste-btn');
+const pasteBtn = document.querySelector("#paste-btn")
 
 async function readFromClipboard() {
-    const clipboardText = await navigator.clipboard.readText();
-    if (clipboardText) {
-        pasteBtn.style.display = "block"
-    } else {
-        pasteBtn.style.display = "none"
-    }
+	const clipboardText = await navigator.clipboard.readText().catch(err => {
+		console.log(`can't read from clipboard:`, err)
+	})
+
+	if (clipboardText) {
+		pasteBtn.style.display = "block"
+	} else {
+		pasteBtn.style.display = "none"
+	}
 }
 
 setInterval(readFromClipboard, 1000)
-pasteBtn.addEventListener('click', paste);
+pasteBtn.addEventListener("click", paste)
+
+function formSubmit(e) {
+	e.preventDefault()
+	const url = e.target.action
+	const method = e.target.method
+	const data = {
+		url: document.getElementById("url").value
+	}
+	axios({ method, url, data }).then(function(res) {
+		console.log(res)
+		if (res.status) {
+			renderTemplate(res.data.data)
+			textContainerElement.style.display = "block"
+		}
+	})
+}
+
+function renderTemplate(temp) {
+	const { brand, productImages, productTitle, productPrice } = temp
+	const targetElement = document.querySelector(".text-container .content")
+	const titleInput = document.createElement("input")
+	titleInput.value = productTitle
+	const propertyInput = document.createElement("input")
+	propertyInput.value = productTitle
+	const innerParagraph = document.createElement("div")
+	innerParagraph.className = "rtl-text"
+
+	innerParagraph.innerText = `
+    برند: ${brand}
+    ---------------
+    خرید و ارسال از اروپا
+    ${getPriceLabel(productPrice)}
+    قیمت جدید + هزینه ارسال از اروپا  85 یورو (تا اطلاع ثانوی)
+    قیمت نهایی به ریال در زمان ثبت سفارش محاسبه می شود
+    ---------------
+    ثبت سفارش پس از واریز 40 درصد قیمت کالا
+    تسویه در زمان دریافت سفارش
+    بررسی موجودیت کالا پیش از واریز وجه
+    محاسبه قیمت ریالی در زمان ثبت سفارش
+`
+
+	targetElement.appendChild(titleInput)
+	targetElement.appendChild(propertyInput)
+	targetElement.appendChild(innerParagraph)
+}
+
+form.addEventListener("submit", formSubmit)
